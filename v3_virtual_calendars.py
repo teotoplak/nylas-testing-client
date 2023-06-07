5793a5bbe66aaa69f836a67ba806df9212a203a7
teotoplak <teotoplak95@gmail.com>
2023-06-07 16:05:11 +0200

    abstracting

--- v3_virtual_calendars.py
+++ v3_virtual_calendars.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+from datetime import timedelta
+
 import requests
 from client import V3_API_KEY
 
-DOMAIN = "https://api-staging.us.nylas.com"
+STAGING_HOST = "https://api-staging.us.nylas.com"
+LOCAL_PASSTHRU_DOMAIN = "http://localhost:8008/"
 HEADERS = {
     'Content-Type': 'application/json',
     'Authorization': f'Bearer {V3_API_KEY}',
@@ -11,7 +15,7 @@ HEADERS = {
 
 def create_grant():
     return requests.post(
-        url=f"{DOMAIN}/v3/grants",
+        url=f"{STAGING_HOST}/v3/grants",
         headers=HEADERS,
         json={
             "provider": "virtual-calendar",
@@ -24,14 +28,14 @@ def create_grant():
 
 def delete_grant(grant_id):
     return requests.delete(
-        url=f"{DOMAIN}/v3/grants/{grant_id}",
+        url=f"{STAGING_HOST}/v3/grants/{grant_id}",
         headers=HEADERS,
     ).json()
 
 
-def create_calendar():
+def create_calendar(url):
     return requests.post(
-        url=f"{DOMAIN}/v3/grants/{grant_id}/calendars",
+        url=f"{url}/calendars",
         headers=HEADERS,
         json={
             "name": "My New Calendar",
@@ -45,9 +49,10 @@ def create_calendar():
     ).json()
 
 
-def create_event():
+def create_event(url):
+    tomorrow_date = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
     return requests.post(
-        url=f"{DOMAIN}/v3/grants/{grant_id}/events?calendar_id={calendar_id}",
+        url=f"{url}/events?calendar_id={calendar_id}",
         headers=HEADERS,
         json={
             "title": "Birthday Party",
@@ -62,12 +67,13 @@ def create_event():
             ],
             "description": "Come ready to skate",
             "when": {
-              "time": 1408875644
+              # "date": tomorrow_date,
+                "time": 1408875644
             },
             "location": "Roller Rink",
             "recurrence": {
               "rrule": [
-                "RRULE:FREQ=WEEKLY;COUNT=8"
+                "RRULE:FREQ=WEEKLY;COUNT=3"
               ],
               "timezone": "America/New_York"
             }
@@ -75,23 +81,31 @@ def create_event():
     ).json()
 
 
-def get_event():
+def get_all_events(url, params):
     return requests.get(
-        url=f"{DOMAIN}/v3/grants/{grant_id}/events/{event_id}?calendar_id={calendar_id}",
+        url=f"{url}/events?calendar_id={calendar_id}",
+        params=params,
         headers=HEADERS,
     ).json()
 
 
-def delete_calendar(calendar_id):
-    return requests.delete(
-        url=f"{DOMAIN}/v3/grants/{grant_id}/calendars/{calendar_id}",
+def get_event(url):
+    return requests.get(
+        url=f"{url}?calendar_id={calendar_id}",
         headers=HEADERS,
     ).json()
 
 
-def delete_event(event_id):
+def delete_calendar(url, calendar_id):
     return requests.delete(
-        url=f"{DOMAIN}/v3/grants/{grant_id}/events/{event_id}?calendar_id={calendar_id}",
+        url=f"{url}/calendars/{calendar_id}",
+        headers=HEADERS,
+    ).json()
+
+
+def delete_event(url, event_id):
+    return requests.delete(
+        url=f"{url}?calendar_id={calendar_id}",
         headers=HEADERS,
     ).json()
 
@@ -104,28 +118,40 @@ if __name__ == '__main__':
     calendar_id = None
     event_id = None
 
-    try:
-        res = create_grant()
-        print(f"created grant: {res}")
-        grant_id = res['data']['id']
+    host = "staging"
+    # host = "passthru"
+    url = None
 
-        res = create_calendar()
+    try:
+
+        if host == "staging":
+            res = create_grant()
+            print(f"created grant: {res}")
+            grant_id = res['data']['id']
+            url = f"{STAGING_HOST}/v3/grants/{grant_id}"
+
+        res = create_calendar(url)
         print(f"created calendar: {res}")
         calendar_id = res['data']['id']
 
-        res = create_event()
+        res = create_event(url)
         print(f"created event: {res}")
         event_id = res['data']['id']
 
-        res = get_event()
+        res = get_event(url)
         print(f"get event: {res}")
+
+        res = get_all_events(url, {
+            "expand_recurring": "true",
+        })
+        print(f"get all events: {res}")
 
     finally:
         if event_id:
-            res = delete_event(event_id)
+            res = delete_event(url, event_id)
             print(f"deleted event: {res}")
         if calendar_id:
-            res = delete_calendar(calendar_id)
+            res = delete_calendar(url, calendar_id)
             print(f"deleted calendar: {res}")
         if grant_id:
             res = delete_grant(grant_id)
