3f9c534b90357b2db1c36870de34dd54925d7f6c
teotoplak <teotoplak95@gmail.com>
2023-07-10 10:38:11 +0200

    supporting prod

--- v3_virtual_calendars.py
+++ v3_virtual_calendars.py
@@ -6,6 +6,7 @@ from client import V3_API_KEY
 from testing_recurring_events import exdate_format
 
 STAGING_HOST = "https://api-staging.us.nylas.com"
+PROD_HOST = "https://api.us.nylas.com"
 LOCAL_PASSTHRU_DOMAIN = "http://localhost:6060"
 HEADERS = {
     'Content-Type': 'application/json',
@@ -67,9 +68,9 @@ TEST_EVENT = {
 }
 
 
-def create_grant():
+def create_grant(host_url):
     return requests.post(
-        url=f"{STAGING_HOST}/v3/grants",
+        url=f"{host_url}/v3/grants",
         headers=HEADERS,
         json={
             "provider": "virtual-calendar",
@@ -174,38 +175,46 @@ if __name__ == '__main__':
     calendar_id = None
     event_id = None
 
-    # host = "staging"
-    host = "local"
+    host = "staging"
+    # host = "prod"
+    # host = "local"
     url = None
+
+    e2e = (host == "staging") or (host == "prod")
 
     try:
 
         if host == "staging":
-            res = create_grant()
+            res = create_grant(STAGING_HOST)
             print(f"created grant: {res}")
             grant_id = res['data']['id']
             url = f"{STAGING_HOST}/v3/grants/{grant_id}"
+        if host == "prod":
+            res = create_grant(PROD_HOST)
+            print(f"created grant: {res}")
+            grant_id = res['data']['id']
+            url = f"{PROD_HOST}/v3/grants/{grant_id}"
         if host == "local":
             url = f"{LOCAL_PASSTHRU_DOMAIN}/v3"
 
         res = create_calendar(url)
         print(f"created calendar: {res}")
-        if host == "staging":
+        if e2e:
             res = res['data']
         calendar_id = res['id']
 
         res = get_calendar(url, calendar_id)
-        if host == "staging":
+        if e2e:
             res = res['data']
         print(f"get calendar: {res}")
 
         res = update_calendar(url, calendar_id)
-        if host == "staging":
+        if e2e:
             res = res['data']
         print(f"update calendar: {res}")
 
         res = get_calendar(url, calendar_id)
-        if host == "staging":
+        if e2e:
             res = res['data']
         print(f"get updated calendar: {res}")
 
@@ -213,19 +222,19 @@ if __name__ == '__main__':
             "metadata_pair": "key1:foo",
         })
         print(f"get all calendars: {res}")
-        if host == "staging":
+        if e2e:
             res = res['data']
         assert len(res) == 1
 
         res = create_event(url)
         print(f"created event: {res}")
-        if host == "staging":
+        if e2e:
             res = res['data']
         event_id = res['id']
 
         res = get_event(url, event_id)
         print(f"get event: {res}")
-        if host == "staging":
+        if e2e:
             res = res['data']
 
         new_event = TEST_EVENT
@@ -238,12 +247,12 @@ if __name__ == '__main__':
         new_event['metadata'] = METADATA_NEW
         res = update_event(url, calendar_id, event_id, new_event)
         print(f"updated event: {res}")
-        if host == "staging":
+        if e2e:
             res = res['data']
         assert res['participants'][0]['status'] == 'no'
 
         res = get_event(url, event_id)
-        if host == "staging":
+        if e2e:
             res = res['data']
         print(f"get updated event: {res}")
 
@@ -254,7 +263,7 @@ if __name__ == '__main__':
             "expand_recurring": "true",
         })
         print(f"get all events: {res}")
-        if host == "staging":
+        if e2e:
             res = res['data']
         print(f"get all events count: {len(res)}")
 
@@ -262,7 +271,7 @@ if __name__ == '__main__':
             "metadata_pair": "key1:foo",
         })
         print(f"get all events with metadata filter: {res}")
-        if host == "staging":
+        if e2e:
             res = res['data']
         assert len(res) == 1
 
@@ -274,7 +283,7 @@ if __name__ == '__main__':
         if calendar_id:
             res = delete_calendar(url, calendar_id)
             print(f"deleted calendar: {res}")
-        if host == "staging" and grant_id:
+        if e2e and grant_id:
             res = delete_grant(grant_id)
             print(f"deleted grant: {res}")
 
